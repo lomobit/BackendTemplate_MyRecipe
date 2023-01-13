1d9e9d09feb56e9379bcf77c941308d4bae89627
vghslnk <vghslnk@vk.com>
2023-01-13 04:09:15 +0300

    Добавление проекта пользовательского интерфейса

---
+++ src/MyRecipeWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyRecipeWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
