4c4527602101179ef318130cda104ca9f4e9a118
vghslnk <vghslnk@vk.com>
2023-01-16 01:07:19 +0300

    Добавление мигратора

--- src/Infrastructure/MyRecipe.Migrator/Program.cs
+++ src/Infrastructure/MyRecipe.Migrator/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyRecipe.Infrastructure;
+using MyRecipe.Migrator;
+
+
+await CreateHostBuilder(args)
+    .RunConsoleAsync(options => options.SuppressStatusMessages = true);
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    new HostBuilder()
+        .ConfigureAppConfiguration((hostContext, configApp) =>
+        {
+            configApp.SetBasePath(Directory.GetCurrentDirectory());
+            configApp.AddJsonFile("appsettings.json");
+            configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json");
+            configApp.AddCommandLine(args);
+            configApp.AddEnvironmentVariables();
+        })
+        .ConfigureServices((hostContext, services) =>
+        {
+            var connectionString = hostContext.Configuration.GetConnectionString("MyRecipeDb");
+
+            services.AddDbContext<MyRecipeDbContext>();
+            services.AddHostedService<MigrationsWorker>();
+        });
